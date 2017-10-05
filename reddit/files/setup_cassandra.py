7af26aa17df372f334079382955c3fb62008c5fa
Tobias Macey <tmacey@mit.edu>
2017-10-05 16:41:08 -0400

    Updated post_install to work more reliably

---
+++ reddit/files/setup_cassandra.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import pycassa
+sys = pycassa.SystemManager("cassandra.service.consul:9160")
+
+if "reddit" not in sys.list_keyspaces():
+    print "creating keyspace 'reddit'"
+    sys.create_keyspace("reddit", "SimpleStrategy", {"replication_factor": "1"})
+    print "done"
+
+if "permacache" not in sys.get_keyspace_column_families("reddit"):
+    print "creating column family 'permacache'"
+    sys.create_column_family("reddit", "permacache")
+    print "done"
