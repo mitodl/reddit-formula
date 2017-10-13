f342e5aded4fa8699738c0a7b0e68c6a2f8ee9e0
Tobias Macey <tmacey@mit.edu>
2017-10-13 14:20:45 -0400

    Increased replication factor for reddit keyspace

--- reddit/files/setup_cassandra.py
+++ reddit/files/setup_cassandra.py
@@ -5,7 +5,7 @@ sys = pycassa.SystemManager("cassandra.service.consul:9160")
 
 if "reddit" not in sys.list_keyspaces():
     print "creating keyspace 'reddit'"
-    sys.create_keyspace("reddit", "SimpleStrategy", {"replication_factor": "1"})
+    sys.create_keyspace("reddit", "SimpleStrategy", {"replication_factor": "2"})
     print "done"
 
 if "permacache" not in sys.get_keyspace_column_families("reddit"):
